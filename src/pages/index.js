39a964dfcbcfc981c53e6fb2026a37fe2a7eb537
Geusan31 <edurais.ariadaffa@gmail.com>
2023-10-08 21:27:33 +0700

    add link in navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,9 +9,8 @@ export default function Home() {
     <nav className='flex bg-slate-300 justify-between font-semibold text-black items-center w-full px-10 py-5'>
       <h1 className='text-lg'>Learn Next JS</h1>
       <ul className='flex justify-between items-center gap-8'>
-        <li className='text-blue-600'>Home</li>
-        <li>About</li>
-        <li>Services</li>
+        <li className='text-blue-600'><Link href={'/'}>Home</Link></li>
+        <li><Link href={'/about'}>About</Link></li>
       </ul>
     </nav>
   )
